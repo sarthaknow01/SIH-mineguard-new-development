5af55a825920324454ec56a0d7e3d9e67e84b9a7
Sarthak Lakudzode <sarthaknow01@gmail.com>
2026-08-31 03:21:23 +0530

    Configure new Supabase database pojjhykcgoyjdqhvjsst

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const SUPABASE_URL = 'https://ylnttsxhxzqurkbpxswe.supabase.co';
-export const SUPABASE_KEY = 'sb_publishable_FJBmuqcCsXKRy6pJeJrXTg_5HCzqKPW';
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://pojjhykcgoyjdqhvjsst.supabase.co';
+export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || 'sb_publishable_AJolqYX51PRdvqmkgaywew_d14CtFzl';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
